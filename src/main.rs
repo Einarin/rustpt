948f5556885ed3924d28b8b535cd024084e95486
Johnny Dickinson <kc0itv@gmail.com>
2016-12-12 23:18:37 -0500

    Autosave after each pass

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::io::Write;
 mod vec3; //split into it's own file for readability
 use vec3::*;
 use std::thread::sleep;
+use std::thread::spawn;
 use std::time::{Duration, Instant};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize,Ordering};
@@ -548,25 +549,29 @@ pub fn main() {
     let mut count = 1;
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
-            samples_per_pass = std::cmp::min(1024,samples_per_pass * 2);
-            println!("Pass {} with {} samples",count,samples_per_pass);
-            count += 1;
+            print!("Pass {} with {} samples",count,samples_per_pass);
             tracer.render(samples_per_pass);
+            count += 1;
+            samples_per_pass = std::cmp::min(1024,samples_per_pass * 2);
             clear([0.5, 0.0, 1.0, 1.0], g);
             let buffer = tracer.buffer.iter().flat_map(|pixel|{let p = *pixel * (1.0/(tracer.samples as f64)); vec![to_u8(p.x),to_u8(p.y),to_u8(p.z),255]}).collect();
             let img = image::ImageBuffer::from_raw(width,height,buffer).unwrap();
             texture.update(&mut g.encoder,&img).unwrap();
             draw_image(&texture, c.transform.scale(0.5,0.5), g);
+            let rgb_buffer: Vec<u8> = tracer.buffer.iter().flat_map(|pixel|{let p = *pixel * (1.0/(tracer.samples as f64)); vec![to_u8(p.x),to_u8(p.y),to_u8(p.z)]}).collect();
+            println!("...Saved");
+            spawn(move || {
+                image::save_buffer(&Path::new("save.png"), &rgb_buffer, width, height, image::RGB(8)).unwrap();
+            });
         });
     }
-    print!("\rSaving...");
+    
     /*let f = File::create("image.ppm").unwrap();
     let mut writer = BufWriter::new(f);
     write!(&mut writer,"P3\n{} {}\n{}\n",width,height,255).unwrap();
     for pixel in tracer.buffer {
         write!(&mut writer," {} {} {} ",to_int(pixel.x),to_int(pixel.y),to_int(pixel.z)).unwrap();
     }*/
-    let buffer: Vec<u8> = tracer.buffer.iter().flat_map(|pixel|{let p = *pixel * (1.0/(tracer.samples as f64)); vec![to_u8(p.x),to_u8(p.y),to_u8(p.z)]}).collect();
-    image::save_buffer(&Path::new("image.png"), &buffer, width, height, image::RGB(8)).unwrap();
-    
+    let rgb_buffer: Vec<u8> = tracer.buffer.iter().flat_map(|pixel|{let p = *pixel * (1.0/(tracer.samples as f64)); vec![to_u8(p.x),to_u8(p.y),to_u8(p.z)]}).collect();
+    image::save_buffer(&Path::new("image.png"), &rgb_buffer, width, height, image::RGB(8)).unwrap();
 }
